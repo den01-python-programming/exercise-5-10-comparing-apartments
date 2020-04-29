9c08bb67a5a40fc294751349e59c69f01da0da6d
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-29 10:35:45 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,10 @@
 import pytest
-import src.exercise
+from src.apartment import Apartment
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    apt_one = Apartment(1, 15, 5200)
+    apt_two = Apartment(2, 37, 4000)
+
+    assert not apt_one.larger_than(apt_two)
+    assert apt_one.price_difference(apt_two) == 70000
+    assert apt_two.more_expensive_than(apt_one)
